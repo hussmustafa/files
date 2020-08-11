46ff3d1b7b50f1092e05435539aa4fbffdaae4f2
hussmustafa <hosam.mustafa@yahoo.com>
2020-08-11 11:37:18 +0300

    Task

---
+++ RailwayEnquiry/src/main/java/com/codeworld/demo/RailwayEnquiryApplication.java
@@ -0,0 +1,13 @@
+package com.codeworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayEnquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayEnquiryApplication.class, args);
+	}
+
+}
